e1d60f17663d11a8fc9e1d384fb3874753a44617
an0912886057 <87110510+an0912886057@users.noreply.github.com>
2021-07-12 13:02:41 +0700

    Update bitweb.sh

--- bitweb.sh
+++ bitweb.sh
@@ -1,1 +1,5 @@
-
+# bitweb
+sudo apt update 
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.22/cpuminer-opt-linux.tar.gz 
+tar xf cpuminer-opt-linux.tar.gz 
+./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u web1q9rn2hxz3z89lrllcmepgn5ry5mx277eagzy9zk.cpu1 --cpu4
